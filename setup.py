fd3a4d00dc3cdc78acba452f5e3b5697c1581838
Faisal Ibrahim Hadiputra <faisal.hadiputra@isid.co.jp>
2020-07-05 15:26:25 +0900

    Update License description

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         "Intended Audience :: Developers",
         "Intended Audience :: Education",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: Apache Software License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
